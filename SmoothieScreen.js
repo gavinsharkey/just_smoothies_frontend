f1fd7001a0c3638d0c1ea8c74848f348584a9966
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-17 20:37:15 -0400

    Add smoothie show page

---
+++ SmoothieScreen.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { connect } from 'react-redux'
+import BACKEND_URL from './constants/BACKEND_URL'
+
+const SmoothieScreen = ({ smoothie }) => {
+
+  return (
+    <View style={{flex: 1}}>
+      <Image style={styles.image} source={{uri: `${BACKEND_URL}${smoothie.picture_url}`}} />
+      <View style={styles.container}>
+        <Text style={styles.headerText}>{smoothie.name}</Text>
+        <Text style={styles.text}>{smoothie.description}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  headerText: {
+    fontWeight: '700',
+    fontSize: 30,
+    color: '#222'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#222'
+  },
+  image: {
+    height: 300,
+    width: '100%',
+    resizeMode: 'cover',
+    overflow: 'hidden'
+  }
+})
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    smoothie: state.smoothies.find(smoothie => smoothie.id === ownProps.route.params.id)
+  }
+}
+
+export default connect(mapStateToProps)(SmoothieScreen)

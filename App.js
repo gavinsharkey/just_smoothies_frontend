af4110b57f00f4f2fe61325a525412a7d81e8936
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-17 20:35:42 -0400

    Configure React Navigation Configure Redux

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Permissions from 'expo-permissions'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import reducer from './reducers/rootReducer'
+import HomeScreen from './HomeScreen'
+import SmoothieScreen from './SmoothieScreen'
+import SmoothieFormScreen from './SmoothieFormScreen'
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} initialParams={{ newPost: false }} />
+            <Stack.Screen name="Smoothie" component={SmoothieScreen} options={{headerShown: false}} />
+            <Stack.Screen name="SmoothieForm" component={SmoothieFormScreen} options={{title: 'Add Smoothie'}} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+      <StatusBar />
+    </Provider>
+    );
+}

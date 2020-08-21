0f92e563b0c016ff42d501d441da66ed2d8a4834
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-21 09:02:06 -0400

    Add start_refreshing dispatch to fetchSmoothies action

--- actions/smoothieActions.js
+++ actions/smoothieActions.js
@@ -2,6 +2,7 @@ import BACKEND_URL from '../constants/BACKEND_URL'
 
 const fetchSmoothies = () => {
   return dispatch => {
+    dispatch({type: 'START_REFRESHING'})
     fetch(`${BACKEND_URL}/api/v1/smoothies`)
     .then(resp => resp.json())
     .then(json => dispatch({type: 'SET_SMOOTHIES', payload: json}))
